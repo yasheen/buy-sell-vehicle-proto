f896833d5b60b7bec5a29623d080af3ef114e11a
Harry Trimble <harry.trimble@network.rca.ac.uk>
2015-09-11 16:21:07 +0100

    routing and browser bar storage

--- app/routes.js
+++ app/routes.js
@@ -1,6 +1,28 @@
 module.exports = {
   bind : function (app) {
 
+  	app.use(function (req, res, next) {
+
+     var viewData = {};
+
+     // Store common vars
+
+     viewData.formData = "";
+     viewData.formQuery = "?";
+
+     for (var name in req.query){
+       var value = req.query[name];
+       viewData.formData += '<input type="hidden" name="'+name+'" value="' + value + '">\n';
+       viewData.formQuery += name + "=" + value + "&";
+     }
+
+     viewData.formQuery = viewData.formQuery.slice(0,-1);
+
+     res.locals(viewData);
+     next();
+     
+   });
+
     app.get('/', function (req, res) {
       res.render('index');
     });
@@ -13,9 +35,27 @@ module.exports = {
 		}
     });
 
-    app.get('/examples/template-data', function (req, res) {
-      res.render('examples/template-data', { 'name' : 'Foo' });
+    app.get('/private-or-trade', function (req, res) {
+      if (req.query["buyer-or-seller"] == 'seller') {
+			res.redirect('verify' + res.locals.formQuery);
+		} else {
+			res.render('private-or-trade/');
+		}
     });
+
+    app.get('/share-code', function (req, res) {
+    	if (req.query["buyer-or-seller"] == 'seller') {
+    		var secondParty="buyer";
+    		var secondPartyCapital="Buyer";
+    	}	else {
+    		var secondParty="seller";
+    		var secondPartyCapital="Seller";
+    	}
+      res.render('share-code', { 'second-party' : secondParty, 'second-party-capital' : secondPartyCapital });
+
+    });
+
+
 
     // add your routes here
 
