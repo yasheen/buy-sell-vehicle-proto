d95c1e57404e30e1586928320fae023ec55654a2
Harry Trimble <harry.trimble@network.rca.ac.uk>
2015-09-11 13:47:09 +0100

    added routing for code page

--- app/routes.js
+++ app/routes.js
@@ -5,6 +5,14 @@ module.exports = {
       res.render('index');
     });
 
+    app.get('/code-routes', function (req, res) {
+      if (req.query.radiogroup == 'no') {
+			res.render('code-no-find-vehicle/');
+		} else {
+			res.render('code-yes-find-vehicle/');
+		}
+    });
+
     app.get('/examples/template-data', function (req, res) {
       res.render('examples/template-data', { 'name' : 'Foo' });
     });
